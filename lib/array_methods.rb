219679a0511f6155b1ccaae630d70b2698bceba3
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-10 13:34:57 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,32 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end 
+  end 
+  nil
 end
 
 def find_max_value(array)
-  sortedArray = array.sort
-  
-  sortedArray.pop
+  max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  max
 end
 
 def find_min_value(array)
-  sortedArray = array.sort
-  
-  sortedArray.shift
+  min = 0
+    array.length.times do |count|
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  min
 end
