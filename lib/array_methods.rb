74d99ac108d0387ce7c6d20e497ffba4e944b864
Fangfei Chen <fangfei1star@gmail.com>
2019-10-21 17:45:10 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,14 @@
 def find_element_index(array, value_to_find)
     # counter = 0
     # while array[counter] do
-  array.index(value_to_find)
-    #   counter+=1
-    # end 
+  # array.index(value_to_find) #or this is the short answer 
+  # counter+=1
+    for index in (0 ... array.length) do
+      if array[index] == value_to_find
+        return index 
+      end
+    end 
+    nil
 end
 
 
