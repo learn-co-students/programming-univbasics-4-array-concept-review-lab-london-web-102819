188eb12bbffe96004fc26ae1572a5ed63238c08b
Fangfei Chen <fangfei1star@gmail.com>
2019-10-21 16:33:38 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+    # counter = 0
+    # while array[counter] do
+  array.index(value_to_find)
+    #   counter+=1
+    # end 
 end
 
+
+
+
+
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array .min
 end
